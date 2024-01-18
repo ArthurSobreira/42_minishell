827eeb6a9d228d24815455e85a7730aafd56208a
Arthur Sobreira <tutusobreira@gmail.com>
2024-01-18 11:55:03 -0300

    Update start.c

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:07:42 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/18 11:50:53 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/01/18 11:53:48 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ void	built_in_array(t_minishell *core)
 	core->built_in[5] = "env";
 	core->built_in[6] = "exit";
 	core->built_in[7] = NULL;
+	
 }
