06df2a928d29ddc42eb4826f3481106633c93397
phenriq2 <pedro.modesto15@gmail.com>
2024-01-20 19:32:16 -0300

    SPLITE_INPUT:first input split

--- src/input.c
+++ src/input.c
@@ -6,12 +6,37 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:15:36 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/20 17:04:39 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/20 19:30:32 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	get_executable(t_minishell *core)
+void	splite_input(t_minishell *core)
 {
+	char	*spl;
+	t_bool	quote;
+	int		start;
+	int		end;
+	int		i;
+
+	quote = FALSE;
+	start = 0;
+	i = 0;
+	spl = core->input;
+	while (spl[i])
+	{
+		if (spl[i] == '\'' || spl[i] == '\"')
+			quote = !quote;
+		if ((spl[i] == ' ') && !quote && (spl[i + 1] != ' '))
+		{
+			end = i;
+			ft_lstadd_back(&core->splited_input, ft_lstnew(ft_substr(spl, start,
+						end - start)));
+			start = i + 1;
+		}
+		i++;
+	}
+	ft_lstadd_back(&core->splited_input, ft_lstnew(ft_substr(spl, start, end
+				- start)));
 }
