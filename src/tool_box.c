1da83cd36cb92d579ad35df3239cf725a34dc756
phenriq2 <pedro.modesto15@gmail.com>
2024-01-20 12:23:21 -0300

    TOOL_BOX:strip function finished

--- src/tool_box.c
+++ src/tool_box.c
@@ -3,36 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   tool_box.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 10:34:03 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/18 12:08:18 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/01/20 12:19:24 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_lstrip(char **str)
+void	ft_strip(char *str)
 {
-	char	*tmp;
+	size_t	len;
+	size_t	start;
+	size_t	end;
 
-	tmp = *str;
-	while (*tmp == ' ')
-		tmp++;
-	*str = tmp;
-}
-
-void	ft_rstrip(char **str)
-{
-	char	*tmp;
-
-	tmp = *str;
-	while (*tmp)
-		tmp++;
-	tmp--;
-	while (*tmp == ' ')
-		tmp--;
-	tmp++;
-	*tmp = '\0';
-	*str = tmp;
+	if (str == NULL || *str == '\0')
+		return ;
+	len = ft_strlen(str);
+	start = 0;
+	end = len - 1;
+	while (start < len && (str[start] == ' ' || str[start] == '\t'
+			|| str[start] == '\n' || str[start] == '\v' || str[start] == '\f'
+			|| str[start] == '\r'))
+		start++;
+	while (end > start && (str[end] == ' ' || str[end] == '\t'
+			|| str[end] == '\n' || str[end] == '\v' || str[end] == '\f'
+			|| str[end] == '\r'))
+		end--;
+	ft_memmove(str, str + start, end - start + 1);
+	str[end - start + 1] = '\0';
 }
