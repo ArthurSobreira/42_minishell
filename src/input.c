0cd41bcd425b009c17dd8b4c89df3d45cc85d1c8
phenriq2 <pedro.modesto15@gmail.com>
2024-01-20 19:37:41 -0300

    SPLITE_INPUT:isspace

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:15:36 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/20 19:30:32 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/20 19:34:55 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	splite_input(t_minishell *core)
 	{
 		if (spl[i] == '\'' || spl[i] == '\"')
 			quote = !quote;
-		if ((spl[i] == ' ') && !quote && (spl[i + 1] != ' '))
+		if (ft_isspace(spl[i]) && !quote && (ft_isspace(spl[i + 1])))
 		{
 			end = i;
 			ft_lstadd_back(&core->splited_input, ft_lstnew(ft_substr(spl, start,
