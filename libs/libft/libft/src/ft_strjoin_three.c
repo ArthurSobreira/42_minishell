043c002a7437e08f6fc4d7f1cb87c4a3aa509f1a
Arthur Sobreira <tutusobreira@gmail.com>
2024-01-24 16:29:07 -0300

    LIBFT: update ft_strjoin_three.c

--- libs/libft/libft/src/ft_strjoin_three.c
+++ libs/libft/libft/src/ft_strjoin_three.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 15:41:00 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/01/24 15:42:56 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/01/24 16:26:07 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,3 @@ char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
 	result = ft_strjoin(result, s3);
 	return (result);
 }
-
