dc5e788564c69df3a9b0e884708a1d3a83cc80a6
phenriq2 <pedro.modesto15@gmail.com>
2024-01-29 15:00:36 -0300

    TOKEN:finally a promising token function

--- src/token.c
+++ src/token.c
@@ -6,26 +6,68 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:26:04 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/29 11:23:28 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/29 13:08:39 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	ft_print_list(t_list *list)
+static void	ft_print_list(void *list)
 {
-	while (list)
-	{
-		printf("%s\n", (char *)list->content);
-		list = list->next;
-	}
+	t_token	token_list;
+
+	token_list = *(t_token *)list;
+	printf("%s\n", token_list.value);
+	printf("%d\n", token_list.type);
 }
 
-void	tokenization(void)
+t_token_type	set_token_type(char *str)
 {
-	t_minishell *core;
-	int i;
+	if (!ft_strcmp(str, "||"))
+		return (TOKEN_OR);
+	if (!ft_strcmp(str, "&&"))
+		return (TOKEN_AND);
+	if (!ft_strcmp(str, ">>"))
+		return (TOKEN_APPEND);
+	if (!ft_strcmp(str, ">"))
+		return (TOKEN_REDIRECT);
+	if (!ft_strcmp(str, "<"))
+		return (TOKEN_REDIRECT_REVERSE);
+	if (!ft_strcmp(str, "|"))
+		return (TOKEN_PIPE);
+	if (!ft_strcmp(str, "&"))
+		return (TOKEN_BACKGROUND);
+	if (!ft_strcmp(str, "<<"))
+		return (TOKEN_HERE_DOC);
+	if (str[0] == '\"')
+		return (TOKEN_QUOTE);
+	return (TOKEN_WORD);
+}
 
-	core = get_core();
+void	tokenization(t_minishell *core)
+{
+	t_token	token;
+	int		i;
+
 	i = 0;
+	while (core->splited_input)
+	{
+		if (!ft_isprint(*(char *)(core->splited_input)->content))
+		{
+			core->splited_input = (core->splited_input)->next;
+			continue ;
+		}
+		token.value = (char *)(core->splited_input)->content;
+		if (core->splited_input->next && !ft_strcmp(token.value,
+				(char *)(core->splited_input->next)->content))
+		{
+			token.value = ft_strjoin(token.value,
+				(char *)(core->splited_input->next)->content);
+			core->splited_input = (core->splited_input)->next;
+		}
+		token.type = set_token_type(token.value);
+		ft_lstadd_back(&core->token_list, ft_lstnew(&token));
+		core->splited_input = (core->splited_input)->next;
+		ft_print_list(core->token_list->content);
+	}
 }
