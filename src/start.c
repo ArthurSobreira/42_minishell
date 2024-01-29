5a929eb1473aa3cee739aeb19f5ec626a2399b7d
Arthur Sobreira <tutusobreira@gmail.com>
2024-01-29 19:18:27 -0300

    START: add init_prompt.c

--- src/start.c
+++ src/start.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   start.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:07:42 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/29 14:29:48 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/29 19:05:21 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_prompt	*init_prompt(void)
+{
+	t_prompt	*prompt_data;
+
+	prompt_data = (t_prompt *)malloc(sizeof(t_prompt));
+	if (!prompt_data)
+		return (NULL);
+	prompt_data->user = NULL;
+	prompt_data->hostname = NULL;
+	prompt_data->current_dir = NULL;
+	prompt_data->prompt = NULL;
+	return (prompt_data);
+}
 
 void	built_in_array(t_minishell *core)
 {
