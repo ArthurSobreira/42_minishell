9fa869693bd31813e251dcc669d65408b59c9c35
Arthur Sobreira <tutusobreira@gmail.com>
2024-01-29 11:20:59 -0300

    LIBFT: Update ft_strjoin_three.c

--- libs/libft/libft/src/ft_strjoin_three.c
+++ libs/libft/libft/src/ft_strjoin_three.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 15:41:00 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/01/24 16:26:07 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/01/29 09:55:35 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
 {
+	char	*temp;
 	char	*result;
 
-	result = ft_strjoin(s1, s2);
-	result = ft_strjoin(result, s3);
+	temp = ft_strjoin(s1, s2);
+	result = ft_strjoin(temp, s3);
+	ft_free_pointer((void *)&temp);
 	return (result);
 }
