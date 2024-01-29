ee613a153b82db01d3c5cfe8ae6a259756532c93
phenriq2 <pedro.modesto15@gmail.com>
2024-01-29 14:59:45 -0300

    start:removing double initiation from pointers

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:07:42 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/29 11:22:22 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:29:48 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	built_in_array(t_minishell *core)
 {
-	t_token	*token;
-
-	core->splited_input = NULL;
 	core->built_in[0] = "echo";
 	core->built_in[1] = "cd";
 	core->built_in[2] = "pwd";
@@ -25,8 +22,6 @@ void	built_in_array(t_minishell *core)
 	core->built_in[5] = "env";
 	core->built_in[6] = "exit";
 	core->built_in[7] = NULL;
-	token = NULL;
-	core->token_list->content = token;
 }
 
 void	print_ascii(void)
