d211939b902a3c1dbf28ef2b2e8dcbda5a3e9d96
phenriq2 <pedro.modesto15@gmail.com>
2024-01-31 17:56:21 -0300

    INCLUDES/STRUCTS: common naming changes

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:16:06 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/01/30 15:44:08 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/01/31 17:46:27 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 typedef struct s_cmd
 {
-	char				*executable;
-	char				**arguments;
-	int					background;
-}						t_cmd;
+	char			*executable;
+	char			**arguments;
+	int				background;
+}					t_cmd;
 
 typedef struct s_exits
 {
-	int					exit_code;
-	char				*exit_msg;
-}						t_exits;
+	int				exit_code;
+	char			*exit_msg;
+}					t_exits;
 
-typedef enum e_token_type
+typedef enum e_tkn_type
 {
 	TOKEN_WORD,
 	TOKEN_REDIRECT,
@@ -36,38 +36,39 @@ typedef enum e_token_type
 	TOKEN_HERE_DOC,
 	TOKEN_QUOTE,
 	TOKEN_BACKGROUND,
+	TOKEN_SEMICOLON,
 	TOKEN_OR,
 	TOKEN_AND
-}						t_token_type;
+}					t_tkn_type;
 
-typedef struct s_list_ms
+typedef struct s_input
 {
-	char				*content;
-	struct s_list_ms	*next;
-}						t_list_ms;
+	char			*content;
+	struct s_input	*next;
+}					t_input;
 typedef struct s_token
 {
-	char				*value;
-	t_token_type		type;
-	struct s_token		*next;
+	char			*value;
+	t_tkn_type		type;
+	struct s_token	*next;
 
-}						t_token;
+}					t_token;
 
 typedef struct s_prompt
 {
-	char				*user;
-	char				*hostname;
-	char				*current_dir;
-	char				*prompt;
-}						t_prompt;
+	char			*user;
+	char			*hostname;
+	char			*current_dir;
+	char			*prompt;
+}					t_prompt;
 
 typedef struct s_minishell
 {
-	t_token				*token_list;
-	t_list_ms			*splited_input;
-	char				*built_in[8];
-	char				*input;
-	t_exits				exits;
-}						t_minishell;
+	t_token			*token_list;
+	t_input			*splited_input;
+	char			*built_in[8];
+	char			*input;
+	t_exits			exits;
+}					t_minishell;
 
 #endif
