99f2baeeab9d52466ce37e8836f95ceda7b3299b
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-06 18:29:35 -0300

    INIT: Update init_structs.c

--- src/init/init_structs.c
+++ src/init/init_structs.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 19:31:03 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/01 23:41:25 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/06 15:12:40 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,22 @@ void	init_minishell(t_minishell *core)
 	core->token_list = NULL;
 	core->splited_input = NULL;
 	core->env_vars = NULL;
+	core->cmd_list = NULL;
 	core->env_vars_size = 0;
 	// core.built_in = NULL;
 	core->input = NULL;
+}
+
+t_cmd	*init_cmd(void)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	cmd->executable = NULL;
+	cmd->arguments = NULL;
+	cmd->redir_in = NULL;
+	cmd->redir_out = NULL;
+	return (cmd);
 }
 
 t_prompt	init_prompt(void)
