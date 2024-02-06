45fbf869bd7e328cd7436aa2505b81b68d4b9c88
phenriq2 <pedro.modesto15@gmail.com>
2024-02-06 19:35:52 -0300

    SRC/DEBBUGER/PRINT_COISAS.C folder for debug code files

--- src/debbuger/print_coisas.c
+++ src/debbuger/print_coisas.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 16:39:32 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/06 16:40:10 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/06 19:08:36 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,14 @@ void	print_token(t_token *token)
 		printf("type: %d\n", tmp->type);
 		printf("value: %s\n", tmp->value);
 		tmp = tmp->next;
+	}
+}
+
+void	print_splited(t_input *input)
+{
+	while (input)
+	{
+		printf("%s\n", input->content);
+		input = input->next;
 	}
 }
