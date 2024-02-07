72288f050c193920c0a95e2f3b1754fd34943f2b
phenriq2 <pedro.modesto15@gmail.com>
2024-02-07 20:15:06 -0300

    SRC/EXPANSION/LOOK_FOR_VARIABLE: fixing error

--- src/expansion/look_for_variable.c
+++ src/expansion/look_for_variable.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 19:20:22 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/07 10:45:34 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/07 15:36:00 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,16 @@ void	find_var(char *str)
 	}
 }
 
-t_bool	is_quote(char c)
-{
-	return (c == '\'');
-}
+// void	look_for_variables(void)
+// {
+// 	t_token	*token;
 
-void	look_for_variables(void)
-{
-	t_token	*token;
+// 	token = get_core()->token_list;
+// 	while (token)
+// 	{
+// 		if ((token->type == TOKEN_WORD || token->type == TOKEN_DQUOTE))
 
-	token = get_core()->token_list;
-	while (token)
-	{
-		if ((token->type == TOKEN_WORD || token->type == TOKEN_DQUOTE))
-		{
-			if (ft_strchr(token->value, '$') && token->type == TOKEN_WORD)
-				find_var(token->value + 1);
-		}
-		token = token->next;
-	}
-}
+// 		}
+// 		token = token->next;
+// 	}
+// }
