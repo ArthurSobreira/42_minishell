b9c7a9ced7c36194a866a07d9a3d0e668ea74aee
phenriq2 <pedro.modesto15@gmail.com>
2024-02-12 18:30:17 -0300

    SRC/TOKENIZER:commented, for testing

--- src/tokenizer/token.c
+++ src/tokenizer/token.c
@@ -6,92 +6,92 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:26:04 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/08 18:39:49 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/12 11:21:25 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_tkn_type	set_tkn_type(char *str)
-{
-	if (!ft_strcmp(str, "||"))
-		return (TOKEN_OR);
-	if (!ft_strcmp(str, "&&"))
-		return (TOKEN_AND);
-	if (!ft_strcmp(str, ">>"))
-		return (TOKEN_APPEND);
-	if (!ft_strcmp(str, ">"))
-		return (TOKEN_REDIRECT);
-	if (!ft_strcmp(str, "<"))
-		return (TOKEN_REDIRECT_REVERSE);
-	if (!ft_strcmp(str, "|"))
-		return (TOKEN_PIPE);
-	if (!ft_strcmp(str, "&"))
-		return (TOKEN_BACKGROUND);
-	if (!ft_strcmp(str, "<<"))
-		return (TOKEN_HERE_DOC);
-	if (!ft_strcmp(str, ";"))
-		return (TOKEN_SEMICOLON);
-	if (str[0] == '\"')
-		return (TOKEN_DQUOTE);
-	if (str[0] == '\'')
-		return (TOKEN_SQUOTE);
-	return (TOKEN_WORD);
-}
+// t_tkn_type	set_tkn_type(char *str)
+// {
+// 	if (!ft_strcmp(str, "||"))
+// 		return (TOKEN_OR);
+// 	if (!ft_strcmp(str, "&&"))
+// 		return (TOKEN_AND);
+// 	if (!ft_strcmp(str, ">>"))
+// 		return (TOKEN_APPEND);
+// 	if (!ft_strcmp(str, ">"))
+// 		return (TOKEN_REDIRECT);
+// 	if (!ft_strcmp(str, "<"))
+// 		return (TOKEN_REDIRECT_REVERSE);
+// 	if (!ft_strcmp(str, "|"))
+// 		return (TOKEN_PIPE);
+// 	if (!ft_strcmp(str, "&"))
+// 		return (TOKEN_BACKGROUND);
+// 	if (!ft_strcmp(str, "<<"))
+// 		return (TOKEN_HERE_DOC);
+// 	if (!ft_strcmp(str, ";"))
+// 		return (TOKEN_SEMICOLON);
+// 	if (str[0] == '\"')
+// 		return (TOKEN_DQUOTE);
+// 	if (str[0] == '\'')
+// 		return (TOKEN_SQUOTE);
+// 	return (TOKEN_WORD);
+// }
 
-t_token	*new_token(char *str)
-{
-	t_token	*token;
+// t_token	*new_token(char *str)
+// {
+// 	t_token	*token;
 
-	token = (t_token *)malloc(sizeof(t_token));
-	token->value = ft_strdup(str);
-	token->type = set_tkn_type(str);
-	token->next = NULL;
-	token->prev = NULL;
-	return (token);
-}
+// 	token = (t_token *)malloc(sizeof(t_token));
+// 	token->value = ft_strdup(str);
+// 	token->type = set_tkn_type(str);
+// 	token->next = NULL;
+// 	token->prev = NULL;
+// 	return (token);
+// }
 
-void	add_token(t_token **head, t_token *new)
-{
-	t_token	*tmp;
+// void	add_token(t_token **head, t_token *new)
+// {
+// 	t_token	*tmp;
 
-	if (!*head)
-	{
-		*head = new;
-		return ;
-	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	new->prev = tmp;
-}
+// 	if (!*head)
+// 	{
+// 		*head = new;
+// 		return ;
+// 	}
+// 	tmp = *head;
+// 	while (tmp->next)
+// 		tmp = tmp->next;
+// 	tmp->next = new;
+// 	new->prev = tmp;
+// }
 
-void	tokenization(void)
-{
-	t_input	*tmp;
-	t_token	*token;
-	char	*str;
-	char	dup;
+// void	tokenization(void)
+// {
+// 	t_input	*tmp;
+// 	t_token	*token;
+// 	char	*str;
+// 	char	dup;
 
-	split_input();
-	if (!get_core()->error_msg)
-		return ;
-	tmp = get_core()->splited_input;
-	while (tmp)
-	{
-		str = ft_strdup(tmp->content);
-		if (tmp->next && !ft_strcmp(tmp->next->content, str))
-		{
-			dup = str[0];
-			ft_free(str);
-			str = ft_strduplicate_char(dup);
-			tmp = tmp->next;
-		}
-		token = new_token(str);
-		add_token(&get_core()->token_list, token);
-		tmp = tmp->next;
-		ft_free(str);
-	}
-	// look_for_variables();
-}
+// 	split_input();
+// 	if (!get_core()->error_msg)
+// 		return ;
+// 	tmp = get_core()->splited_input;
+// 	while (tmp)
+// 	{
+// 		str = ft_strdup(tmp->content);
+// 		if (tmp->next && !ft_strcmp(tmp->next->content, str))
+// 		{
+// 			dup = str[0];
+// 			ft_free(str);
+// 			str = ft_strduplicate_char(dup);
+// 			tmp = tmp->next;
+// 		}
+// 		token = new_token(str);
+// 		add_token(&get_core()->token_list, token);
+// 		tmp = tmp->next;
+// 		ft_free(str);
+// 	}
+// 	look_for_variables();
+// }
--- src/tokenizer/token_utils.c
+++ src/tokenizer/token_utils.c
@@ -6,118 +6,118 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 11:35:51 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/08 17:40:40 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/12 11:21:02 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_input	*new_node(char *str)
-{
-	t_input	*new;
+// t_input	*new_node(char *str)
+// {
+// 	t_input	*new;
 
-	new = malloc(sizeof(t_input));
-	if (!new)
-		return (NULL);
-	new->content = str;
-	new->next = NULL;
-	return (new);
-}
+// 	new = malloc(sizeof(t_input));
+// 	if (!new)
+// 		return (NULL);
+// 	new->content = str;
+// 	new->next = NULL;
+// 	return (new);
+// }
 
-void	split_input(void)
-{
-	int		i;
-	char	quote;
-	int		start;
+// void	split_input(void)
+// {
+// 	int		i;
+// 	char	quote;
+// 	int		start;
 
-	i = -1;
-	ft_strip(get_core()->input);
-	while (get_core()->input[++i])
-	{
-		if (get_core()->input[i] == '\'' || get_core()->input[i] == '\"')
-		{
-			quote = get_core()->input[i];
-			start = i;
-			while (get_core()->input[++i] != quote
-				&& get_core()->input[i] != '\0')
-				;
-			if (get_core()->input[i] == '\0')
-			{
-				ft_error("syntax error: unexpected end of file\n", 2);
-				return ;
-			}
-			add_to_list(get_core(), start, i + 1);
-		}
-		else if (get_core()->input[i] != ' ')
-			process_non_space(get_core(), &i);
-	}
-}
+// 	i = -1;
+// 	ft_strip(get_core()->input);
+// 	while (get_core()->input[++i])
+// 	{
+// 		if (get_core()->input[i] == '\'' || get_core()->input[i] == '\"')
+// 		{
+// 			quote = get_core()->input[i];
+// 			start = i;
+// 			while (get_core()->input[++i] != quote
+// 				&& get_core()->input[i] != '\0')
+// 				;
+// 			if (get_core()->input[i] == '\0')
+// 			{
+// 				ft_error("syntax error: unexpected end of file\n", 2);
+// 				return ;
+// 			}
+// 			add_to_list(get_core(), start, i + 1);
+// 		}
+// 		else if (get_core()->input[i] != ' ')
+// 			process_non_space(get_core(), &i);
+// 	}
+// }
 
-void	process_non_space(t_minishell *core, int *i)
-{
-	int		start;
-	char	quote;
+// void	process_non_space(t_minishell *core, int *i)
+// {
+// 	int		start;
+// 	char	quote;
 
-	start = *i;
-	while (isall(core, i))
-	{
-		if (core->input[*i] == '\'' || core->input[*i] == '\"')
-		{
-			quote = core->input[*i];
-			while (core->input[++(*i)] != quote && core->input[*i] != '\0')
-				;
-		}
-		else
-			(*i)++;
-	}
-	add_to_list(core, start, *i);
-	while (ft_ispipe(core->input[*i]) || ft_issemicolon(core->input[*i])
-		|| ft_isredir(core->input[*i]) || ft_isbackground(core->input[*i]))
-	{
-		add_to_list(core, *i, *i + 1);
-		(*i)++;
-	}
-	(*i)--;
-}
+// 	start = *i;
+// 	while (isall(core, i))
+// 	{
+// 		if (core->input[*i] == '\'' || core->input[*i] == '\"')
+// 		{
+// 			quote = core->input[*i];
+// 			while (core->input[++(*i)] != quote && core->input[*i] != '\0')
+// 				;
+// 		}
+// 		else
+// 			(*i)++;
+// 	}
+// 	add_to_list(core, start, *i);
+// 	while (ft_ispipe(core->input[*i]) || ft_issemicolon(core->input[*i])
+// 		|| ft_isredir(core->input[*i]) || ft_isbackground(core->input[*i]))
+// 	{
+// 		add_to_list(core, *i, *i + 1);
+// 		(*i)++;
+// 	}
+// 	(*i)--;
+// }
 
-void	add_to_list(t_minishell *core, int start, int end)
-{
-	char	*str;
-	t_input	*tmp;
-	t_input	*new;
+// void	add_to_list(t_minishell *core, int start, int end)
+// {
+// 	char	*str;
+// 	t_input	*tmp;
+// 	t_input	*new;
 
-	str = ft_substr(core->input, start, end - start);
-	if (!ft_isprint(str[0]))
-	{
-		ft_free(str);
-		return ;
-	}
-	new = new_node(str);
-	if (!core->splited_input)
-	{
-		core->splited_input = new;
-		return ;
-	}
-	tmp = core->splited_input;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-}
+// 	str = ft_substr(core->input, start, end - start);
+// 	if (!ft_isprint(str[0]))
+// 	{
+// 		ft_free(str);
+// 		return ;
+// 	}
+// 	new = new_node(str);
+// 	if (!core->splited_input)
+// 	{
+// 		core->splited_input = new;
+// 		return ;
+// 	}
+// 	tmp = core->splited_input;
+// 	while (tmp->next)
+// 		tmp = tmp->next;
+// 	tmp->next = new;
+// }
 
-t_bool	is_redir_token(t_token *token)
-{
-	if (token->type == TOKEN_REDIRECT ||
-		token->type == TOKEN_APPEND ||
-		token->type == TOKEN_REDIRECT_REVERSE ||
-		token->type == TOKEN_HERE_DOC)
-		return (TRUE);
-	return (FALSE);
-}
+// t_bool	is_redir_token(t_token *token)
+// {
+// 	if (token->type == TOKEN_REDIRECT ||
+// 		token->type == TOKEN_APPEND ||
+// 		token->type == TOKEN_REDIRECT_REVERSE ||
+// 		token->type == TOKEN_HERE_DOC)
+// 		return (TRUE);
+// 	return (FALSE);
+// }
 
-t_bool	isall(t_minishell *core, int *i)
-{
-	return (!ft_isspace(core->input[*i]) && !ft_ispipe(core->input[*i])
-		&& !ft_issemicolon((core->input[*i])) && !ft_isredir(core->input[*i])
-		&& !ft_isbackground(core->input[*i]) && core->input[*i] != '\"'
-		&& core->input[*i] != '\'' && core->input[*i] != '\0');
-}
+// t_bool	isall(t_minishell *core, int *i)
+// {
+// 	return (!ft_isspace(core->input[*i]) && !ft_ispipe(core->input[*i])
+// 		&& !ft_issemicolon((core->input[*i])) && !ft_isredir(core->input[*i])
+// 		&& !ft_isbackground(core->input[*i]) && core->input[*i] != '\"'
+// 		&& core->input[*i] != '\'' && core->input[*i] != '\0');
+// }
