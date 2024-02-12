425c96a71c3a37a3ac2cc53b54d02ce4bf38cf19
phenriq2 <pedro.modesto15@gmail.com>
2024-02-12 18:20:34 -0300

    SRC/EXIT/ERROR: added garbage collector

--- src/exit/clear.c
+++ src/exit/clear.c
@@ -6,28 +6,28 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:16:52 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/07 12:23:35 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/12 15:27:12 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_clear_token(void)
-{
-	t_minishell	*core;
-	t_token		*tmp;
+// void	ft_clear_token(void)
+// {
+// 	t_minishell	*core;
+// 	t_token		*tmp;
 
-	core = get_core();
-	if (core->token_list == NULL)
-		return ;
-	while (core->token_list)
-	{
-		tmp = core->token_list->next;
-		ft_free(core->token_list->value);
-		ft_free(core->token_list);
-		core->token_list = tmp;
-	}
-}
+// 	core = get_core();
+// 	if (core->token_list == NULL)
+// 		return ;
+// 	while (core->token_list)
+// 	{
+// 		tmp = core->token_list->next;
+// 		ft_free(core->token_list->value);
+// 		ft_free(core->token_list);
+// 		core->token_list = tmp;
+// 	}
+// }
 
 void	ft_clear_env_vars(void)
 {
@@ -47,23 +47,23 @@ void	ft_clear_env_vars(void)
 	}
 }
 
-void	ft_clear_splited_input(void)
-{
-	t_minishell	*core;
-	t_input		*tmp;
+// void	ft_clear_splited_input(void)
+// {
+// 	t_minishell	*core;
+// 	t_input		*tmp;
 
-	core = get_core();
-	if (core->splited_input == NULL)
-		return ;
-	while (core->splited_input)
-	{
-		tmp = core->splited_input->next;
-		ft_free(core->splited_input->content);
-		ft_free(core->splited_input);
-		core->splited_input = tmp;
-	}
-	ft_clear_token();
-}
+// 	core = get_core();
+// 	if (core->splited_input == NULL)
+// 		return ;
+// 	while (core->splited_input)
+// 	{
+// 		tmp = core->splited_input->next;
+// 		ft_free(core->splited_input->content);
+// 		ft_free(core->splited_input);
+// 		core->splited_input = tmp;
+// 	}
+// 	ft_clear_token();
+// }
 
 void	ft_clear_redir_in(t_redir_in **redir_in)
 {
