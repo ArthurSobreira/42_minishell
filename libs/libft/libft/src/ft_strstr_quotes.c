5557ae0704f00090250c3a7e785b9d8938dff1b7
phenriq2 <pedro.modesto15@gmail.com>
2024-02-14 17:53:57 -0300

    LIBFT: att adding functions for expansion

---
+++ libs/libft/libft/src/ft_strstr_quotes.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr_quotes.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 09:07:52 by phenriq2          #+#    #+#             */
+/*   Updated: 2024/02/14 10:23:07 by phenriq2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	skip_quotes(char *str, size_t *i)
+{
+	char	quote;
+
+	quote = str[*i];
+	(*i)++;
+	while (str[*i] && str[*i] != quote)
+		(*i)++;
+	if (str[*i] == quote)
+		(*i)++;
+}
+
+char	*ft_strstr_quotes(const char *big, const char *little)
+{
+	size_t	counter;
+	size_t	index;
+
+	counter = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[counter])
+	{
+		if (big[counter] == '\'')
+			skip_quotes((char *)big, &counter);
+		if (!big[counter])
+			break ;
+		index = 0;
+		while (big[counter + index] && little[index] && big[counter
+				+ index] == little[index])
+			index++;
+		if (!little[index])
+			return ((char *)&big[counter]);
+		counter++;
+	}
+	return (NULL);
+}
