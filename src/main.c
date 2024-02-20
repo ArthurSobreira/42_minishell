b74e00f6dbeb3bfe3ea97fa5a6856148d3816ddc
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-20 15:44:26 -0300

    MAIN: Update core definition

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 10:52:05 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/15 19:03:20 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/20 12:35:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	t_minishell	*core;
 
-	core = NULL;
 	(void)argv;
+	core = NULL;
 	if (argc == 1)
 	{
 		core = get_core();
