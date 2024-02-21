e9a612b6f4583610fa9f6d84755023a2793e4d7c
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-21 18:29:25 -0300

    EXIT: Update error.c

--- src/exit/error.c
+++ src/exit/error.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 10:34:03 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/13 17:10:48 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/21 12:11:54 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,16 @@ void	ft_error(char *str, int status)
 	core->exit_status = status;
 }
 
-void	ft_file_error(char *file_name, char *str, int status)
+void	ft_file_error(char *file_name, char *str, int status, size_t index)
 {
 	t_minishell	*core;
 	char		*msg;
 	char		*tmp_file_name;
 
 	core = get_core();
-	if (core->error_check.file_error == TRUE)
+	if (core->error_check.file_error[index] == TRUE)
 		return ;
-	core->error_check.file_error = TRUE;
+	core->error_check.file_error[index] = TRUE;
 	tmp_file_name = ft_strjoin(file_name, ": ");
 	msg = ft_strjoin_three("minishell: ", tmp_file_name, str);
 	ft_putstr_fd(msg, STDERR_FILENO);
