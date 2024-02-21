d8b4b6d8e544dea5684d01867a2c80ea61a67129
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-21 18:55:18 -0300

    EXECUTOR: Update command_table.c

--- src/executor/command_table.c
+++ src/executor/command_table.c
@@ -6,80 +6,112 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 18:03:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/19 17:37:03 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/21 16:46:31 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	command_executor(void)
+void	create_cmd_table(void)
 {
 	t_minishell	*core;
-	
+	t_token		*current_tkn;
+	size_t		index;
+
+	index = -1;
 	core = get_core();
-	core->cmd_table = malloc(sizeof(t_cmd *) * core->pipe_count + 1);
-	if (core->pipe_count == 0)
+	core->pipe_count = count_pipes();
+	core->cmd_table = init_cmd_table();
+	if (core->cmd_table == NULL)
+		return ;
+	while (++index <= core->pipe_count)
 	{
-		core->cmd_table[0] = init_cmd();
-		handle_redirects(core->cmd_table[0]);
-		core->cmd_table[0]->cmd = get_command();
-		
-		printf("Command: %s\n", core->cmd_table[0]->cmd);
-		print_token(get_core()->token_list);
-
-		// print_redir_in(core->cmd_table->redir_in);
-		// print_redir_out(core->cmd_table->redir_out);
-
-		// remove_unnecessary_redir_in(&core->cmd_table->redir_in);
-		// remove_unnecessary_redir_out(&core->cmd_table->redir_out);
-
-		// printf("After remove unnecessary\n");
-
-		// print_redir_in(core->cmd_table->redir_in);
-		// print_redir_out(core->cmd_table->redir_out);
-
-		// print_token(get_core()->token_list);
-
-		// exec_single_cmd();
+		handle_redirects(&core->cmd_table[index], index);
+		remove_unnecessary_redir_in(&core->cmd_table[index].redir_in);
+		remove_unnecessary_redir_out(&core->cmd_table[index].redir_out);
+		current_tkn = core->token_list;
+		if (current_tkn && current_tkn->type == TOKEN_PIPE)
+		{
+			remove_token(&core->token_list, current_tkn);
+			ft_clear_redir_in(&core->cmd_table[index].redir_in);
+			ft_clear_redir_out(&core->cmd_table[index].redir_out);
+			current_tkn = core->token_list;
+			continue ;
+		}
+		if (core->error_check.file_error[index])
+		{
+			core->error_check.cmd_error = TRUE;
+			while (current_tkn && current_tkn->type != TOKEN_PIPE)
+			{
+				remove_token(&core->token_list, current_tkn);
+				ft_clear_redir_in(&core->cmd_table[index].redir_in);
+				ft_clear_redir_out(&core->cmd_table[index].redir_out);
+				current_tkn = core->token_list;
+			}
+			continue ;
+		}
+		if (current_tkn != NULL)
+		{
+			if (is_builtin(current_tkn->value))
+			{
+				core->cmd_table[index].is_builtin = TRUE;
+				core->cmd_table[index].cmd = ft_strdup(current_tkn->value);
+				ft_clear_redir_in(&core->cmd_table[index].redir_in);
+				remove_token(&core->token_list, current_tkn);
+			}
+			else
+				core->cmd_table[index].cmd = get_command();
+			core->cmd_table[index].args = \
+				get_arguments(core->cmd_table[index].cmd);
+			// core->cmd_table[index].envp = NULL;
+		}
 	}
+	print_cmd_table(core->cmd_table);
 }
 
-// void 	create_cmd_table(void)
-// {
-// 	t_token		*current_tkn;
-// 	t_cmd		*cmd;
-// 	int			pipe_count;
-
-// 	pipe_count = count_pipes();
-// 	get_core()->pipe_count = pipe_count;
-// 	current_tkn = get_core()->token_list;
-// 	while (current_tkn)
-// 	{
-// 		cmd = init_cmd();
-// 		if (current_tkn->type == TOKEN_PIPE)
-// 			current_tkn = current_tkn->next;
-// 		cmd->cmd = ft_strdup(current_tkn->value);
-// 		cmd->args = create_args(current_tkn);
-// 		cmd->envp = create_envp();
-// 		cmd->redir_in = NULL;
-// 		cmd->redir_out = NULL;
-// 		add_cmd_to_table(cmd);
-// 		current_tkn = current_tkn->next;
-// 	}
-// }
-
-int	count_pipes(void)
+char	*get_command(void)
 {
 	t_token		*current_tkn;
-	int			pipe_count;
+	t_token		*next_tkn;
+	char		*cmd_name;
+	char		*cmd_path;
 
 	current_tkn = get_core()->token_list;
-	pipe_count = 0;
-	while (current_tkn)
+	next_tkn = current_tkn->next;
+	cmd_name = ft_strdup(current_tkn->value);
+	if (access(cmd_name, F_OK | X_OK) == 0)
 	{
-		if (current_tkn->type == TOKEN_PIPE)
-			pipe_count++;
-		current_tkn = current_tkn->next;
+		remove_token(&get_core()->token_list, current_tkn);
+		return (cmd_name);
 	}
-	return (pipe_count);
+	cmd_path = validate_cmd_path(cmd_name);
+	remove_token(&get_core()->token_list, current_tkn);
+	if (cmd_path == NULL)
+		return (cmd_name);
+	return (cmd_path);
+}
+
+char	**get_arguments(char *cmd_name)
+{
+	t_token		*current_tkn;
+	t_token		*next_tkn;
+	size_t		index;
+	char		**args;
+
+	index = 0;
+	current_tkn = get_core()->token_list;
+	args = malloc(sizeof(char *) * (count_args(current_tkn) + 2));
+	if (!args)
+		return (NULL);
+	args[index++] = ft_strdup(cmd_name);
+	while (current_tkn && current_tkn->type != TOKEN_PIPE)
+	{
+		next_tkn = current_tkn->next;
+		args[index] = ft_strdup(current_tkn->value);
+		remove_token(&get_core()->token_list, current_tkn);
+		current_tkn = next_tkn;
+		index++;
+	}
+	args[index] = NULL;
+	return (args);
 }
