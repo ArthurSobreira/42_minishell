b6ba33cc74641d98b5418f52246ee9326e2370c1
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-22 12:13:41 -0300

    EXECUTOR: Update executor.c

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:45:51 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/20 17:46:05 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:48:00 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,14 @@ void	command_executor(void)
 
 	core = get_core();
 	create_cmd_table();
+	core->cmd_table[0].pid = fork();
+	if (!core->error_check.file_error[0])
+	{
+		if (core->cmd_table[0].pid == 0)
+		{
+			if (execve(core->cmd_table[0].cmd, core->cmd_table[0].args, NULL) == -1)
+				perror("minishell");
+		}
+	}
+	waitpid(core->cmd_table[0].pid, NULL, 0);
 }
