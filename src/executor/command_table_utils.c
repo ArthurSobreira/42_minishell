be1a16893b4f5a514708b5ed02fc5da6a034a0a8
Arthur Sobreira <tutusobreira@gmail.com>
2024-02-22 10:57:02 -0300

    EXECUTOR: Update command_table_utils.c

--- src/executor/command_table_utils.c
+++ src/executor/command_table_utils.c
@@ -6,11 +6,58 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:39:30 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/22 09:17:22 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:44:11 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*validate_cmd_path(char *cmd_name)
+{
+	size_t	index;
+	char	*cmd_path;
+	char	*cmd_with_path;
+	char	**split_path;
+
+	index = 0;
+	cmd_path = NULL;
+	cmd_with_path = NULL;
+	split_path = get_split_path();
+	while (split_path[index] != NULL)
+	{
+		cmd_path = ft_strjoin(split_path[index], "/");
+		cmd_with_path = ft_strjoin(cmd_path, cmd_name);
+		free(cmd_path);
+		if (access(cmd_with_path, F_OK | X_OK) == 0)
+		{
+			free_variables(cmd_name, split_path);
+			return (cmd_with_path);
+		}
+		free(cmd_with_path);
+		index++;
+	}
+	free_variables(NULL, split_path);
+	return (NULL);
+}
+
+char	**get_split_path(void)
+{
+	t_var	*env_vars;
+	char	**split_path;
+
+	env_vars = get_core()->env_vars;
+	split_path = NULL;
+	while (env_vars)
+	{
+		if (!ft_strcmp(env_vars->key, "PATH"))
+		{
+			split_path = ft_split(env_vars->value, ':');
+			break ;
+		}
+		env_vars = env_vars->next;
+	}
+	return (split_path);
+}
 
 int	count_pipes(void)
 {
