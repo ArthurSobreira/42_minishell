46be2f2100fd62895ce64f5a1125652985e9761a
phenriq2 <pedro.modesto15@gmail.com>
2024-02-26 17:03:48 -0300

    SRC/BUILT-IN/EXIT.C: exit param added

--- src/built-in/exit.c
+++ src/built-in/exit.c
@@ -6,22 +6,41 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 16:35:52 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/26 15:14:38 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/26 16:56:29 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static t_bool	validate_args(t_minishell *core, t_cmd *command)
+{
+	if (command && ft_isdigit(command->args[1][0]))
+	{
+		core->exit_status = ft_atoi(command->args[1]);
+		printf("%d\n", core->exit_status);
+	}
+	else if (command && !ft_isdigit(command->args[1][0]))
+	{
+		ft_putendl_fd("minishell: exit: numeric argument required",
+			STDERR_FILENO);
+		core->exit_status = SYNTAX_ERROR;
+	}
+	else if (command && ft_matrix_len(command->args) > 2)
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+		return (FALSE);
+	}
+	return (TRUE);
+}
+
 void	exit_shell(t_cmd *command)
 {
 	t_minishell	*core;
 
-	if (command && ft_matrix_len(command->args) > 1)
-	{
-		ft_putendl_fd("exit: too many arguments", STDERR_FILENO);
-		return ;
-	}
 	core = get_core();
+	if (command && command->args[1])
+		if (!validate_args(core, command))
+			return ;
 	if (core->token_list != NULL)
 		ft_clear_token();
 	if (core->env_vars != NULL)
@@ -29,6 +48,6 @@ void	exit_shell(t_cmd *command)
 	if (core->cmd_table != NULL)
 		ft_clear_cmd_table();
 	clear_garbage();
-	ft_error("bye bye\n", EXIT_SUCCESS);
+	ft_error("bye bye", core->exit_status);
 	exit(core->exit_status);
 }
