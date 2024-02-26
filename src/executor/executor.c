392602b0a7eb4e75a73cf525a8f897a5c8cb0037
phenriq2 <pedro.modesto15@gmail.com>
2024-02-26 15:09:41 -0300

    SRC/EXECUTOR/EXECUTOR.C double exit removed

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:45:51 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/23 12:26:33 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/26 11:22:43 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	command_executor(void)
 
 	core = get_core();
 	create_cmd_table();
-	core->cmd_table[0].pid = fork();
 	if (!core->error_check.file_error[0] && !core->cmd_table[0].is_builtin)
 	{
+		core->cmd_table[0].pid = fork();
 		if (core->cmd_table[0].pid == 0)
 		{
-			if (execve(core->cmd_table[0].cmd, \
-				core->cmd_table[0].args, NULL) == -1)
+			if (execve(core->cmd_table[0].cmd,
+					core->cmd_table[0].args, NULL) == -1)
 				perror("minishell");
 		}
+		waitpid(core->cmd_table[0].pid, NULL, 0);
 	}
-	waitpid(core->cmd_table[0].pid, NULL, 0);
 }
