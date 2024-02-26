d7a6d6a506588b05ffe83c42e7276b5b940767eb
phenriq2 <pedro.modesto15@gmail.com>
2024-02-26 17:04:35 -0300

    SRC/EXECUTOR/EXECUTOR.C: basic tests

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:45:51 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/02/26 15:17:07 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/26 16:41:14 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,7 @@ void	command_executor(void)
 			execute_builtin(&core->cmd_table[0]);
 		// else
 		// 	execute_single_command(&core->cmd_table[0]);
-		return ;
 	}
-	core->cmd_table[0].pid = fork();
-	if (!core->error_check.file_error[0] && !core->cmd_table[0].is_builtin)
-	{
-		core->cmd_table[0].pid = fork();
-		if (core->cmd_table[0].pid == 0)
-		{
-			if (core->cmd_table[0].cmd != NULL)
-			if (execve(core->cmd_table[0].cmd, \
-				core->cmd_table[0].args, core->cmd_table[0].envp) == -1)
-			{
-				perror("minishell");
-			}
-		}
-	}
-	waitpid(core->cmd_table[0].pid, NULL, 0);
 }
 
 void	execute_builtin(t_cmd *command)
