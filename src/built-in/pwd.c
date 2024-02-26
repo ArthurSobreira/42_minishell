4db3237d2ff8a5f458a00f5e68e080399f85b21a
phenriq2 <pedro.modesto15@gmail.com>
2024-02-26 17:04:03 -0300

    SRC/BUILT-IN/PWD.c:fd change

--- src/built-in/pwd.c
+++ src/built-in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:22:56 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/02/26 14:57:15 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/02/26 16:44:14 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,6 @@ void	print_working_directory(t_cmd *command)
 	char	*current_dir;
 	int		fd_out;
 
-	if (ft_matrix_len(command->args) > 1)
-	{
-		ft_putendl_fd("pwd: too many arguments", STDERR_FILENO);
-		return ;
-	}
 	fd_out = STDOUT_FILENO;
 	if (command->redir_out)
 		fd_out = command->redir_out->fd_out;
