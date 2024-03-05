e49e1080c1d536775fb98e09e6adede1f670b9b7
Arthur Sobreira <tutusobreira@gmail.com>
2024-03-05 18:55:51 -0300

    HEADER: Update main header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:13:42 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/03/05 16:46:29 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/03/05 18:50:43 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -192,6 +192,7 @@ void		handle_intermediate_proc(t_minishell *core, t_cmd *command);
 void		handle_final_proc(t_minishell *core, t_cmd *command);
 void		backup_fd_in_out(int fd_backup[2]);
 void		restore_fd_in_out(void);
+void		backup_pipe_fd(void);
 t_bool		is_empty_cmd(t_cmd *cmd);
 
 // Signal functions
