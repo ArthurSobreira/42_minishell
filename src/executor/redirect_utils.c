d66ac021788d7fc4d2a6d43df8da5869262a255a
Arthur Sobreira <tutusobreira@gmail.com>
2024-03-06 11:05:58 -0300

    EXECUTOR: Update redirect_utils.c

--- src/executor/redirect_utils.c
+++ src/executor/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 18:46:42 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/03/05 18:50:57 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/03/06 10:51:00 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,16 @@ void	restore_fd_in_out(void)
 	dup2(core->fd_backup[1], STDOUT_FILENO);
 	close(core->fd_backup[0]);
 	close(core->fd_backup[1]);
+}
+
+void	close_all_fds(void)
+{
+	size_t	index;
+
+	index = 4;
+	while (index <= 1024)
+	{
+		close(index);
+		index++;
+	}
 }
