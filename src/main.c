003a4f590d890d4e58ea7c8fe96b62611c979939
Arthur Sobreira <tutusobreira@gmail.com>
2024-03-18 09:36:16 -0300

    MAIN: Add save_tty function

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 10:52:05 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/03/08 10:24:29 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/03/10 20:15:50 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ t_minishell	*get_core(void)
 	return (&core);
 }
 
+void	save_tty(int tty_fd)
+{
+	static struct termios	tty;
+
+	if (!tty_fd)
+		tcgetattr(STDIN_FILENO, &tty);
+	else
+		tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_minishell	*core;
@@ -27,15 +37,14 @@ int	main(int argc, char *argv[], char *envp[])
 	core = NULL;
 	if (argc == 1)
 	{
+		save_tty(0);
 		core = get_core();
 		core->envp = envp;
+		clear_prompt();
+		print_ascii();
 		init_minishell(core);
 		get_env_vars(core);
-		
 		prompt_loop(core);
 	}
 	return (core->exit_status);
 }
-
-// clear_prompt();
-// print_ascii();
